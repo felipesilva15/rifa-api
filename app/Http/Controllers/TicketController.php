16aecef0e07f5640dd81a3ea3e425c588a5bfe86
Felipe Silva <felipe.allware@gmail.com>
2024-11-11 22:47:28 -0300

    docs: Documentação do recurso de ticket com Swagger

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -11,4 +11,193 @@ class TicketController extends Controller
         $this->model = $model;
         $this->request = $request;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/ticket",
+     *      tags={"Ticket"},
+     *      summary="List all tickets",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="Ticket ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="participant_id",
+     *         in="query",
+     *         description="Participant ID",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="raffle_id",
+     *         in="query",
+     *         description="Raffle ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="number",
+     *         in="query",
+     *         description="Ticket number",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="payment_date",
+     *         in="query",
+     *         description="Ticket payment date",
+     *         @OA\Schema(type="string", format="date")
+     *      ),
+     *      @OA\Parameter(
+     *         name="value",
+     *         in="query",
+     *         description="Ticket value",
+     *         @OA\Schema(type="number", format="double")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Ticket list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Ticket")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      )
+     * )
+     */
+    public function index() {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/ticket/{id}",
+     *      tags={"Ticket"},
+     *      summary="List an ticket by ID",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Ticket ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Ticket data",
+     *          @OA\JsonContent(ref="#/components/schemas/Ticket")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      )
+     * )
+     */
+    public function show($id) {
+        return parent::show($id);
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/ticket",
+     *      tags={"Ticket"},
+     *      summary="Registers an ticket",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for creating a new ticket",
+     *         @OA\JsonContent(ref="#/components/schemas/Ticket")
+     *      ),
+     *      @OA\Response(
+     *          response="201", 
+     *          description="Registered ticket data",
+     *          @OA\JsonContent(ref="#/components/schemas/Ticket")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      )
+     * )
+     */
+    public function store(Request $request) {
+        return parent::store($request);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/ticket/{id}",
+     *      tags={"Ticket"},
+     *      summary="Update an ticket",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Ticket ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for update ticket",
+     *         @OA\JsonContent(ref="#/components/schemas/Ticket")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Updated ticket data",
+     *          @OA\JsonContent(ref="#/components/schemas/Ticket")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      )
+     * )
+     */
+    public function update(Request $request, $id) {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/ticket/{id}",
+     *      tags={"Ticket"},
+     *      summary="Deletes an ticket",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Ticket ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="204", 
+     *          description="Response"
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ErrorResponse")
+     *      )
+     * )
+     */
+    public function destroy( $id) {
+        return parent::destroy($id);
+    }
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @OA\Schema(
+ *      schema="Ticket",
+ *      required={"raffle_id", "participant_id", "number"},
+ *      @OA\Property(property="id", type="integer", example=1),
+ *      @OA\Property(property="raffle_id", type="integer", example=1),
+ *      @OA\Property(property="participant_id", type="integer", example=1),
+ *      @OA\Property(property="number", type="integer", example=1, minimum=1),
+ *      @OA\Property(property="payment_date", type="string", format="date", example="2024-12-01"),
+ *      @OA\Property(property="value", type="number", format="double", minimum=0.01, maximum=999.99, example=20),
+ *      @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z"),
+ *      @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z")
+ * )
+ */
 class Ticket extends Model
 {
     protected $fillable = [
