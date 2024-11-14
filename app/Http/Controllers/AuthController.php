a929f737099ce3713f754ff3bc2e77b5cfd50d8f
Felipe Silva <felipe.allware@gmail.com>
2024-11-14 19:58:54 -0300

    feat: Criando controller e rotas implementadas com autenticação

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $credentials = [
+            'email' => $request->email, // Campo personalizado para o login
+            'password' =>$request->password, // Campo padrão para a senha
+        ];
+    
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(["message" => "Credenciais inválidas."], 401);
+        }
+    
+        return $this->respondWithToken($token);
+    }
+    
+    public function me() {
+        return response()->json(auth()->user());
+    }
+    
+    public function logout() {
+        auth()->logout();
+    
+        return response()->json(['message' => 'Logout efetuado com sucesso!']);
+    }
+    
+    public function refresh() {
+        return $this->respondWithToken(auth()->refresh());
+    }
+    
+    protected function respondWithToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RaffleController;
 use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\TicketController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('raffle', RaffleController::class);
-Route::get('raffle/{raffle}/tickets', [RaffleController::class, 'tickets']);
-Route::get('raffle/{raffle}/card', [RaffleController::class, 'card']);
-Route::apiResource('participant', ParticipantController::class);
-Route::apiResource('ticket', TicketController::class);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('raffle', RaffleController::class);
+    Route::get('raffle/{raffle}/tickets', [RaffleController::class, 'tickets']);
+    Route::get('raffle/{raffle}/card', [RaffleController::class, 'card']);
+    Route::apiResource('participant', ParticipantController::class);
+    Route::apiResource('ticket', TicketController::class);
+});
